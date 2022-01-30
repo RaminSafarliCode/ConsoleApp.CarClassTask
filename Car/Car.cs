d2500e7aa04a25455a99193c56828ed2fbc40082
RaminSafarliCode <raminas@code.edu.az>
2022-01-31 00:24:31 +0400

    Add project files.

---
+++ Car/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    internal class Car
+    {
+        /*members*/
+
+        //fields
+        public string city;
+        public string name;
+        public string model;
+        public int year;
+        public string type;
+        public string color;
+        public double engine;
+        public int horsePower;
+        public string fuelType;
+        public double distance;
+        public string gearbox;
+        public string transmission;
+        public double price;
+
+
+        //methods
+
+        public string GetInfo()
+        {
+            string output = $"City: {this.city}\n" +
+                $"Brand: {this.name}\n" +
+                $"Model: {this.model}\n" +
+                $"Release year: {this.year}\n" +
+                $"Ban type: {this.type}\n" +
+                $"Color: {this.color}\n" +
+                $"Engine: {this.engine} L\n" +
+                $"Engine power: {this.horsePower} h.p.\n" +
+                $"Fuel type: {this.fuelType}\n" +
+                $"Distance: {this.distance}km\n" +
+                $"Gearbox type: {this.gearbox}\n" +
+                $"Transmission: {this.transmission}\n" +
+                $"Price: {this.price} ₼";
+            return output;  
+        }
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Text;
+
+namespace Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+            Car car1 = new Car();
+
+            car1.city = "Baku";
+            car1.name = "Lexus";
+            car1.model = "LX 570";
+            car1.year = 2013;
+            car1.type = "Offroader";
+            car1.color = "White";
+            car1.engine = 5.7;
+            car1.horsePower = 383;
+            car1.fuelType = "Gasoline";
+            car1.distance = 70000;
+            car1.gearbox = "Automatic";
+            car1.transmission = "Front";
+            car1.price = 58000;
+
+            Car car2 = new Car();
+
+            car2.city = "Baku";
+            car2.name = "Chevrolet";
+            car2.model = "Cruze";
+            car2.year = 2017;
+            car2.type = "Sedan";
+            car2.color = "Silver";
+            car2.engine = 1.4;
+            car2.horsePower = 153;
+            car2.fuelType = "Gasoline";
+            car2.distance = 215000;
+            car2.gearbox = "Automatic";
+            car2.transmission = "Front";
+            car2.price = 17700;
+
+            Car car3 = new Car();
+
+            car3.city = "Gandja";
+            car3.name = "LADA (VAZ)";
+            car3.model = "2106";
+            car3.year = 1987;
+            car3.type = "Sedan";
+            car3.color = "Cyan";
+            car3.engine = 1.6;
+            car3.horsePower = 75;
+            car3.fuelType = "Gasoline";
+            car3.distance = 470000;
+            car3.gearbox = "Manual";
+            car3.transmission = "Back";
+            car3.price = 3500;
+
+            Car[] cars = new Car[3];
+
+            cars[0] = car1;
+            cars[1] = car2;
+            cars[2] = car3;
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.GetInfo());
+                Console.WriteLine("--------------------------");
+            }
+
+
+
+        }
+    }
+}
